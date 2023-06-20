d40cadb20ddd7537649440f01d555b20a5873b86
AJ <andrew@thiesen.co>
2023-06-20 19:07:48 -0400

    Add Reboot Switch
    Add Reboot Switch

--- NoSleep.sh
+++ NoSleep.sh
@@ -1,4 +1,4 @@
-# Andrew Thiesen
+# Andrew Thiesen | andrew@thiesen.co
 # 20 JUNE 2023
 # Tested on Rockly Linux 9.2
 
@@ -30,3 +30,8 @@ sed -i 's/^#HandleLidSwitch=.*/HandleLidSwitch=ignore/' "$config_file"
 
 echo "HandleLidSwitch option in $config_file has been set to 'ignore'."
 
+# Prompt the user to reboot for the changes to take effect
+read -p "Do you want to reboot the system now? (y/n): " choice
+if [[ $choice =~ ^[Yy]$ ]]; then
+    reboot
+fi
